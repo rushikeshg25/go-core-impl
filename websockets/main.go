aa33bcacbfe88cfca87976e4d22f915ebf74a48c
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2025-01-08 23:48:18 +0530

    init:Ws

--- websockets/main.go
+++ websockets/main.go
@@ -1,10 +1,33 @@
-package main  
+package main
 
 import (
-  "fmt"
+	"fmt"
+	"log"
+	"net"
 )
 
 
 func main(){
-  fmt.Println("hello")
+  fmt.Println("Websocket:")
+  listner,err:=net.Listen("tcp",":8080");
+  if err!=nil{
+	log.Fatal("Tcp Server Error");
+   }
+  for{
+    log.Println("Listening on Port 8080")
+    conn,err:=listner.Accept()
+    if err!=nil{
+      log.Fatal("unable to connect with the client");
+    }
+    go processConn(conn);
+  }
+}
+
+func processConn(conn net.Conn){
+  buf:=make([]byte,1024);
+  _,err:=conn.Read(buf);
+  if err!=nil{
+    log.Fatal("Error Processing the Stream")
+  }
+  log.Printf("%s",buf);
 }
