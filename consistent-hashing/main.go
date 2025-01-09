04f64b44ad131c7dd72bf8fd77f4bf5ad0811e4b
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2025-01-09 12:27:53 +0530

    feat: Consistent hasing ex

--- consistent-hashing/main.go
+++ consistent-hashing/main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
 
+func main() {
+	ch := NewConsistentHash(50)
+	node1 := &StorageNode{Host: "node1"}
+	node2 := &StorageNode{Host: "node2"}
 
-func main(){}
+	ch.AddNode(node1)
+	ch.AddNode(node2)
+
+	item := "item1"
+	assignedNode, _ := ch.Assign(item)
+	fmt.Printf("Item %s is assigned to node %s\n", item, assignedNode.Host)
+
+	ch.Plot(item, node1)
+}
