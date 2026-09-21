23200edbec08f2a693c5330d90aaa125502ce86b
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2026-09-21 13:54:12 +0530

    feat(raft): persist node identity term vote log and committed prefix atomically

--- RAFT/internal/raft/types.go
+++ RAFT/internal/raft/types.go
@@ -15,67 +15,62 @@ const (
 
 type LogEntry struct {
 	Term    int
-	Command interface{}
+	Command []byte
+	Noop    bool
 }
-
 type Raft struct {
-	mu    sync.Mutex
-	peers []string
-	id    int
-
-	currentTerm int
-	votedFor    int
-	log         []LogEntry
-
-	commitIndex int
-	lastApplied int
-
-	nextIndex  []int
-	matchIndex []int
-
-	role        NodeRole
-	heartbeat   time.Duration
-	election    time.Duration
-	lastContact time.Time
-	sendRPC     func(address string, method string, args interface{}, reply interface{}) bool
+	mu                      sync.Mutex
+	peers                   []string
+	id                      int
+	currentTerm, votedFor   int
+	log                     []LogEntry
+	commitIndex             int
+	nextIndex, matchIndex   []int
+	inflight                []bool
+	role                    NodeRole
+	heartbeat, election     time.Duration
+	deadline, lastHeartbeat time.Time
+	sendRPC                 func(string, string, interface{}, interface{}) bool
+	path                    string
+	err                     error
+	started, stopped        bool
+	stop                    chan struct{}
+	wg                      sync.WaitGroup
 }
-
-type RequestVoteArgs struct {
-	Term         int
-	CandidateId  int
-	LastLogIndex int
-	LastLogTerm  int
-}
-
+type RequestVoteArgs struct{ Term, CandidateId, LastLogIndex, LastLogTerm int }
 type RequestVoteReply struct {
 	Term        int
 	VoteGranted bool
 }
-
 type AppendEntriesArgs struct {
-	Term         int
-	LeaderId     int
-	PrevLogIndex int
-	PrevLogTerm  int
-	Entries      []LogEntry
-	LeaderCommit int
+	Term, LeaderId, PrevLogIndex, PrevLogTerm int
+	Entries                                   []LogEntry
+	LeaderCommit                              int
 }
-
 type AppendEntriesReply struct {
-	Term    int
-	Success bool
+	Term      int
+	Success   bool
+	NextIndex int
+}
+type SubmitArgs struct{ Command []byte }
+type SubmitReply struct {
+	Index, Term int
+	Leader      bool
+}
+type StatusArgs struct{}
+type StatusReply struct {
+	Term, CommitIndex, LastLogIndex int
+	Role                            NodeRole
+}
+type AppliedEntry struct {
+	Index   int
+	Command []byte
 }
 
-func NewRaft(id int, peers []string, sendRPC func(string, string, interface{}, interface{}) bool) *Raft {
-	return &Raft{
-		id:          id,
-		peers:       peers,
-		role:        Follower,
-		votedFor:    -1,
-		currentTerm: 0,
-		heartbeat:   100 * time.Millisecond,
-		election:    300 * time.Millisecond,
-		log:         make([]LogEntry, 0),
-		sendRPC:     sendRPC,
+func NewRaft(id int, peers []string, send func(string, string, interface{}, interface{}) bool) *Raft {
+	r, e := NewPersistentRaft(id, peers, send, "")
+	if e != nil {
+		panic(e)
 	}
+	return r
 }
